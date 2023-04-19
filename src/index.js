87093d867fb901c39984d7daa4244ccca78a229c
ManuErCaximba <ercaximbero96@gmail.com>
2023-04-19 22:47:55 +0200

    abstrayendo try/catch

--- src/index.js
+++ src/index.js
@@ -30,19 +30,24 @@ client.on('ready', async (c) => {
 });
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
-    const member = oldState.member || newState.member;
-    if (!member) return;
-
-    if (!newState.channel && oldState.channel) {
-        let data = await fetchData(member);
-        if (data.length === 0) return;
-        saveInfoStored(data, member);
-    } else if (!oldState.channel && newState.channel) {
-        const now = new Date();
-        const startTime = now.getTime();
-        const startDate = now.getDate() + "-" + (now.getMonth() + 1) + "-" + now.getFullYear();
-        users.set(member.id, new DateTuple(startDate, startTime));
+    try {
+        const member = oldState.member || newState.member;
+        if (!member) return;
+    
+        if (!newState.channel && oldState.channel) {
+            let data = await fetchData(member);
+            if (data.length === 0) return;
+            saveInfoStored(data, member);
+        } else if (!oldState.channel && newState.channel) {
+            const now = new Date();
+            const startTime = now.getTime();
+            const startDate = now.getDate() + "-" + (now.getMonth() + 1) + "-" + now.getFullYear();
+            users.set(member.id, new DateTuple(startDate, startTime));
+        }
+    } catch (e) {
+        await logChannel.send(e.stack);
     }
+    
 });
 
 keepAlive();
@@ -62,34 +67,30 @@ async function fetchInfoStored() {
 }
 
 async function saveInfoStored(data, member) {
-    try {
-        const info = new Info(member.id, member.displayName);
-        if (infoStored.length === 0) {
+    const info = new Info(member.id, member.displayName);
+    if (infoStored.length === 0) {
+        info.data = data
+        infoStored = [info];
+        msgPromise = await dataChannel.send(JSON.stringify(infoStored));
+    } else {
+        let elemIndex = await findElementIndex(infoStored, info.id);
+        if (elemIndex === undefined || elemIndex === null) {
             info.data = data
-            infoStored = [info];
-            msgPromise = await dataChannel.send(JSON.stringify(infoStored));
+            infoStored.push(info);
         } else {
-            let elemIndex = await findElementIndex(infoStored, info.id);
-            if (elemIndex === undefined || elemIndex === null) {
-                info.data = data
-                infoStored.push(info);
-            } else {
-                const memberInfoStored = infoStored[elemIndex];
-                const dataUpdated = await updateData(memberInfoStored, data);
-                const isUpdated = JSON.stringify(dataUpdated) === JSON.stringify(data) ? false : true;
-                memberInfoStored.data = await updateDataEntries(memberInfoStored);
-                if (isUpdated) {
-                    memberInfoStored.data.pop();
-                }
-                dataUpdated.forEach((elem) => {
-                    memberInfoStored.data.push(elem);
-                });
-                infoStored.splice(elemIndex, 1, memberInfoStored);
+            const memberInfoStored = infoStored[elemIndex];
+            const dataUpdated = await updateData(memberInfoStored, data);
+            const isUpdated = JSON.stringify(dataUpdated) === JSON.stringify(data) ? false : true;
+            memberInfoStored.data = await updateDataEntries(memberInfoStored);
+            if (isUpdated) {
+                memberInfoStored.data.pop();
             }
-            editMessage(infoStored);
+            dataUpdated.forEach((elem) => {
+                memberInfoStored.data.push(elem);
+            });
+            infoStored.splice(elemIndex, 1, memberInfoStored);
         }
-    } catch (e) {
-        await logChannel.send(e);
+        editMessage(infoStored);
     }
 }
 
@@ -127,25 +128,21 @@ function updateData(memberInfoStored, data) {
 }
 
 async function fetchData(member) {
-    try {
-        const startTime = users.get(member.id).time;
-        const startDate = users.get(member.id).date;
-        if (!startTime) return [];
-    
-        const now = new Date();
-        const endDate = now.getDate() + "-" + (now.getMonth() + 1) + "-" + now.getFullYear();
-        if (startDate === endDate) {
-            const totalTime = now.getTime() - startTime;
-            return [new DateTuple(startDate, totalTime)];
-        } else {
-            const atZeroTime = now;
-            atZeroTime.setHours(0, 0, 0, 0);
-            const todayTime = now.getTime() - atZeroTime;
-            const yesterdayTime = atZeroTime - startTime;
-            return [new DateTuple(startDate, yesterdayTime), new DateTuple(endDate, todayTime)];
-        }
-    } catch (e) {
-        await logChannel.send(e);
+    const startTime = users.get(member.id).time;
+    const startDate = users.get(member.id).date;
+    if (!startTime) return [];
+
+    const now = new Date();
+    const endDate = now.getDate() + "-" + (now.getMonth() + 1) + "-" + now.getFullYear();
+    if (startDate === endDate) {
+        const totalTime = now.getTime() - startTime;
+        return [new DateTuple(startDate, totalTime)];
+    } else {
+        const atZeroTime = now;
+        atZeroTime.setHours(0, 0, 0, 0);
+        const todayTime = now.getTime() - atZeroTime;
+        const yesterdayTime = atZeroTime - startTime;
+        return [new DateTuple(startDate, yesterdayTime), new DateTuple(endDate, todayTime)];
     }
 }
 
