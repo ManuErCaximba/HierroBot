ca9cebac464aba60c23b20313bbfd202cf525c17
ManuErCaximba <ercaximbero96@gmail.com>
2023-04-27 18:52:40 +0200

    Añadido funciones de añadir roles

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const crypto = require('crypto');
 const { createClient } = require('@supabase/supabase-js');
 const { Client, IntentsBitField } = require('discord.js');
 const { Info, DateTuple } = require('./model');
+const schedule = require('node-schedule');
 
 const keepAlive = require('./server');
 
@@ -24,6 +25,8 @@ let supabase;
 client.on('ready', async (c) => {
     console.log(`${c.user.tag} is online.`);
     supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_KEY);
+    schedule.scheduleJob('0 10 */1 */1 *', assignBrontosaurios);
+    schedule.scheduleJob('0 10 1 */1 *', assignForaneos);
 });
 
 client.on('voiceStateUpdate', async (oldState, newState) => {
@@ -107,5 +110,66 @@ function generateDateTuples(info) {
         return infoToSave;
     } catch (e) {
         console.log(e.stack);
+    }
+}
+
+async function assignBrontosaurios() {
+    let { data: userBDIds, error } = await supabase.from('Info').select('id');
+    const rthGuild = await client.guilds.cache.get(process.env.STG_SERVER_ID);
+    const userGuildIds = Array.from((await rthGuild.members.fetch())
+        .filter(m => m._roles.includes('996053691704545362'))
+        .map(m => Number(m.id))
+        .values());
+    const members = Array.from((await rthGuild.members.fetch()).values());
+    const userIds = userBDIds.filter(value => userGuildIds.includes(value.id));
+    const brontosaurio = await rthGuild.roles.fetch('1013023706609635398')
+    const monthAgo = new Date();
+    monthAgo.setDate(monthAgo.getDate() - process.env.DAYS_CHECKED);
+    userIds.forEach(async elem => {
+        const member = await members.filter(m => m.id == elem.id)[0]
+        let { data: millisecs, error } = await supabase.from('DateTuple').select('millisec').eq('memberId', elem.id).gte('date', (monthAgo.toISOString()).toLocaleString('es-ES'));
+        let total = 0;
+        millisecs.forEach(elem => {
+            total += elem.millisec;
+        });
+        const hours = total / 3600000;
+        if (hours >= process.env.BRONTOSAURIO_MIN) {
+            // member.roles.add(brontosaurio);
+            console.log('a')
+        } else {
+            // member.roles.remove(brontosaurio);
+            console.log('a')
+        }
+    });
+}
+
+async function assignForaneos() {
+    const rthGuild = await client.guilds.cache.get(process.env.STG_SERVER_ID);
+    const userGuildIds = Array.from((await rthGuild.members.fetch())
+        .filter(m => m._roles.includes('996053691704545362'))
+        .map(m => Number(m.id))
+        .values());
+    const members = Array.from((await rthGuild.members.fetch()).values());
+    const foraneo = await rthGuild.roles.fetch('1025751725778415638')
+    const monthAgo = new Date();
+    monthAgo.setDate(monthAgo.getDate() - process.env.DAYS_CHECKED);
+    for (var id of userGuildIds) {
+        let { data: userBDIds, error } = await supabase.from('Info').select('id').eq('id', id);
+        const member = await members.filter(m => m.id == id)[0]
+        if (userBDIds.length === 0) {
+            // member.roles.add(foraneo);
+            console.log('b')
+        } else {
+            let { data: millisecs, error } = await supabase.from('DateTuple').select('millisec').eq('memberId', id).gte('date', (monthAgo.toISOString()).toLocaleString('es-ES'));
+            let total = 0;
+            millisecs.forEach(elem => {
+                total += elem.millisec;
+            });
+            const hours = total / 3600000;
+            if (hours <= process.env.FORANEO_MIN) {
+                // member.roles.add(foraneo);
+                console.log('b')
+            }
+        }
     }
 }
